a43841032cc60d925c0767e40427289fed793c4e
Pradhyumn Yadav <73153956+Pradhyumn1@users.noreply.github.com>
2023-05-26 22:17:13 +0530

    some changes

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Setup
+
+npm i && npm start
+
+
+i am working on this project and make data visualization.
+
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ee39915aca429d2487c45e1d84cbc5ec",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
