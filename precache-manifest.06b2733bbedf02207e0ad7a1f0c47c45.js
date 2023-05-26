deb0506c1c42a126a9343bdca223e8d1a8e29865
Pradhyumn Yadav <73153956+Pradhyumn1@users.noreply.github.com>
2023-05-26 22:22:38 +0530

    Updates

---
+++ precache-manifest.06b2733bbedf02207e0ad7a1f0c47c45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb1b4eddb1965a993abd1fd42ce8d14",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "8139e4ba296573cd73b4",
+    "url": "/react-gh-pages/static/css/main.290bbf88.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/react-gh-pages/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8139e4ba296573cd73b4",
+    "url": "/react-gh-pages/static/js/main.137ab4b5.chunk.js"
+  },
+  {
+    "revision": "62fb29af8f345106de5d",
+    "url": "/react-gh-pages/static/js/runtime-main.f14dcfe3.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/react-gh-pages/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "7e7c50db2e676514f5b817979cef6df8",
+    "url": "/react-gh-pages/static/media/background.7e7c50db.jpg"
+  },
+  {
+    "revision": "c9a474cdab9b3c3655799f4d33f010c5",
+    "url": "/react-gh-pages/static/media/city.c9a474cd.jpg"
+  }
+]);
